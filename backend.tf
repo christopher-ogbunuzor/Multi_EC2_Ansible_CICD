d5eaa937dc0aee3b22714f7a432083a71a11f444
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-12-14 12:36:38 +0000

    UPDATE - Configure Nginx on multiple servers in public subnet using CICD

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
       bucket = "my-first-lambdabucket-194694014750"
-      key = "sprint3/week6/ec2-ansible/terraform.tfstate"
+      key = "sprint3/week6/MultiEc2AnsibleCICD/terraform.tfstate"
       dynamodb_table = "terraform-lock"
       
   }
